ae1ee353f65a6dabaf925d5d1d09f055ca6ca78b
romicons <romina.constantinoff@gmail.com>
2024-01-26 23:38:34 -0300

    Added some basic styles.

---
+++ index.js
@@ -0,0 +1,40 @@
+function loadApp() {
+
+	var flipbook = $('.flipbook');
+
+ 	// Check if the CSS was already loaded
+	
+	if (flipbook.width()==0 || flipbook.height()==0) {
+		setTimeout(loadApp, 10);
+		return;
+	}
+
+	$('.flipbook .double').scissor();
+
+	// Create the flipbook
+
+	$('.flipbook').turn({
+			// Elevation
+
+			elevation: 50,
+			
+			// Enable gradients
+
+			gradients: true,
+			
+			// Auto center this flipbook
+
+			autoCenter: true
+
+	});
+}
+
+// Load the HTML4 version if there's not CSS transform
+
+yepnope({
+	test : Modernizr.csstransforms,
+	yep: ['lib/turn.min.js'],
+	nope: ['ib/turn.html4.min.js'],
+	both: ['lib/scissor.min.js', 'css/double-page.css'],
+	complete: loadApp
+});
